1f45dc59275ad5e72c1e4cbb8ea6b5cdcd7a4b53
Ayon <suprovatbit@gmail.com>
2024-08-12 19:29:33 +0600

    bug fix

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -32,6 +32,7 @@ const Photo = () => {
             fill
             alt="profile_photo"
             className="object-contain"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw,33vw"
           />
         </motion.div>
 

43581777be3b261fd194eb7ce1a549d2d1c3a001
Ayon <suprovatbit@gmail.com>
2024-08-28 12:45:56 +0600

    Home Imporvement

--- app/page.jsx
+++ app/page.jsx
@@ -1,47 +1,50 @@
 "use client";
-//Dependencies
+
+// Dependencies
 import { Button } from "@/components/ui/button";
+import dynamic from "next/dynamic";
+import { useCallback } from "react";
 import { FiDownload } from "react-icons/fi";
 
-//Internal Dependencies
+// Internal Dependencies
 import Photo from "@/components/Photo";
-import Socials from "@/components/Socials";
-import Stats from "@/components/Stats";
 
-//Home Page
+// Dynamic Imports
+const Stats = dynamic(() => import("@/components/Stats"));
+const Socials = dynamic(() => import("@/components/Socials"), { ssr: false });
+
+// Home Page
 const Home = () => {
-  //CV File Handler
-  const downloadCvHandler = () => {
-    const cvFile = "/assets/Ayon_Bit_CV.txt"; //cv url
+  // CV File Handler
+  const downloadCvHandler = useCallback(() => {
+    const cvFile = "/assets/Ayon_Bit_CV.txt"; // CV url
 
-    // Create a temporary link element and trigger the download
     const link = document.createElement("a");
     link.href = cvFile;
-    link.download = "Ayon_Bit_CV.txt"; // Set the default file name
+    link.download = "Ayon_Bit_CV.txt";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-  };
+  }, []);
 
   return (
     <section className="min-h-screen overflow-hidden">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          {/*text */}
+          {/* Text */}
           <div className="text-center xl:text-left order-2 xl:order-none ">
             <span className="text-xl">Web Developer</span>
             <h1 className="h1">
               Hello I&apos;m <br />{" "}
               <span className="text-accent">Ayon Bit</span>
             </h1>
-            {/* Change this context for real */}
             <p className="max-w-[500px] mb-9 text-white/80">
               Experienced software developer skilled in multiple programming
               languages, collaborative, and effective at providing technical
               guidance. Dedicated to staying current with industry trends and
               motivated to create high-quality software.
             </p>
-            {/* button and social media*/}
+            {/* Button and Social Media */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button
                 variant="outline"
@@ -63,13 +66,13 @@ const Home = () => {
               </div>
             </div>
           </div>
-          {/* photo*/}
+          {/* Photo */}
           <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
-      {/* Stats*/}
+      {/* Stats */}
       <Stats />
     </section>
   );
